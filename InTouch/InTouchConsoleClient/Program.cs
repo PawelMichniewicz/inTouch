b0b850e7041ce3a25d1ed6f576a79e81c7b22cee
PawelMichniewicz <48495023+PawelMichniewicz@users.noreply.github.com>
2022-10-11 20:47:59 +0200

    Console client project created. User class added.

---
+++ InTouch/InTouchConsoleClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using InTouchConsoleClient;
+using System;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Welcome to inTouch communicator");
+Console.WriteLine("Provide your LOGIN:");
+
+User u = new();
+
+u.Login = Console.ReadLine()?.ToLower();
+
+Console.WriteLine("Provide your Password:");
+
+u.Password = Console.ReadLine()?.ToLower();
+
+Console.WriteLine($"You are now logged in as {u.Login} with password {u.Password}");
---
+++ InTouch/InTouchConsoleClient/User.cs
@@ -0,0 +1,9 @@
+﻿namespace InTouchConsoleClient
+{
+    internal class User
+    {
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
