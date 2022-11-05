3c1465d359d08a9ed3e0f2d75f5d282e55d6ec52
PawelMichniewicz <48495023+PawelMichniewicz@users.noreply.github.com>
2022-11-05 21:49:04 +0100

    First cat for ChatRoom model

--- InTouch/CommunicationWebApi/Models/ChatRoom.cs
+++ InTouch/CommunicationWebApi/Models/ChatRoom.cs
@@ -3,5 +3,15 @@
     public class ChatRoom
     {
         public int ID { get; set; }
+
+        public string? Name { get; set; } = "Chat room";
+
+        public bool Archived { get; set; } = false;
+
+        public bool Deleted { get; set; } = false;
+
+        public IList<User>? Members { get; set; }
+
+        public IList<Message>? Messages { get; set; }
     }
 }
